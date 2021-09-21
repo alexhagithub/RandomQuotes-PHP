1c1ff5cec179826817c5ec63714d684ae5331195
Miguel Huamaní <miguel.huamani@inetum.world>
2021-09-20 23:04:40 -0500

    feature: add pipeline

---
+++ dev-pipeline-devops.groovy
@@ -0,0 +1,59 @@
+@Library('devops-scripts@main')
+import sharedlib.JenkinsfileUtil
+
+def project = "ESMS"
+def product = "esikamultisite"
+static final PROJECT_REPOSITORY = "https://github.com/OctopusSamples/RandomQuotes-PHP.git"
+static final GITHUB_TOKEN_CREDENTIAL = "usergh"
+this.language = 'php'
+this.propertiesName = 'project-test.properties'
+this.propertiesPath = "/tmp/" + this.propertiesName
+def utils = new JenkinsfileUtil(this)
+
+def repositoryUrl = scm.userRemoteConfigs[0].url
+
+println repositoryUrl
+
+try{
+    node{
+        stage('Preparation'){
+            //TODO: DELTAS, CLEAN WS CHECKOUT SCM
+            //git branch: 'master', url: PROJECT_REPOSITORY, credentialsId: GITHUB_TOKEN_CREDENTIAL
+            //git branch: 'master', url: PROJECT_REPOSITORY
+        }
+
+        stage('Unit Testing'){
+            //TODO: JUNIT OR OTHER TEST FW
+        }
+
+        stage('QualityCode'){                        
+            //utils.scanningQualityCode()
+            // Quality Gate
+            sh "echo 'starting QualityGate'"
+            //utils.getQualityGate()
+        }
+
+        stage('Security'){
+            //TODO: GIT LEAKS / AVALORA
+            //utils.scanningSecutiry()
+        }
+
+        stage('Build'){
+            //TODO: BUILD
+        }
+
+        stage('Contenerize'){
+            //TODO: DOCKER BUILD (IF POSSIBLE)
+        }
+
+        stage('Artifact Publish'){
+            //TODO: UP TO NEXUS
+        }
+    }
+}catch (Exception e){
+    node{
+        //utils.notifyByMail('FAIL', recipients) //Generar esta funcion
+        sh 'echo THERE WAS AN ERROR, REPORT TO DEVOPS TEAM!'
+        throw e
+    }
+}
